a635dad412a5d6e75281f192d3c6f9d4d4c9398d
Arslan Ghani <m.arslan6845@gmail.com>
2022-08-07 13:05:04 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// Active bar
+
+let nav= document.querySelector(".navigation-wrap");
+window.onscroll=function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }
+    else{
+        nav.classList.remove("scroll-on");
+
+    }
+}
+
+// Nav Hide (To do responsive for Mobile Devices)
+
+let navbar= document.querySelectorAll('.nav-link');
+let navCollapse= document.querySelector('.navbar-collapse.collapse');
+navbar.forEach(function(e){
+    e.addEventListener('click',function(){
+        navCollapse.classList.remove('show');
+    })
+})
+
+// counter design (for home page section)
+document.addEventListener("DOMContentLoaded", ()=>{
+    function counter(id,start,end,duration){
+        let obj= document.getElementById(id),
+        current=start,
+        range=end-start,
+        increment= end > start ?  1 : -1,
+        step=Math.abs(Math.floor(duration/range)),
+        timer= setInterval(()=>{
+            current +=increment; 
+            obj.textContent=current;
+            if(current == end){
+                clearInterval(timer)
+            }
+        },step)
+
+    }
+    counter("count1",0,1250,300);
+    counter('count2',0,2351,250);
+    counter('count3',0,2165,300);
+    counter('count4',0,1958,300);
+})
